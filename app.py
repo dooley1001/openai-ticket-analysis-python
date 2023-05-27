39979d83fbf2a9350bbcbe489644c601b00ef5bf
dooley1001 <davidlambo@hotmail.com>
2023-05-27 17:13:27 +0100

    First commit

--- app.py
+++ app.py
@@ -10,26 +10,33 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+
+        #Create our model 
+        data = request.form["data"]
         response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=generate_prompt(data),
+            temperature=0,
         )
+
+        #print(data)
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(data):
+    return """Identify the following items from the prompt text: 
+- Is the customer expressing anger? (true or false)
+- Sentiment (negative or positive)
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
-    )
+Format your response as a key value pairs with \
+"Sentiment" & "Anger" as the keys.
+If the information isn't present, use "unknown" \
+as the value.
+Format the Anger value as a boolean.
+
+""".format(
+    data.capitalize() 
+)
