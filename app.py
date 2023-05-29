387b6f3152da2204c815b82c66bb7ec87bf41287
dooley1001 <davidlambo@hotmail.com>
2023-05-29 11:06:40 +0100

    wip

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 import openai
 from flask import Flask, redirect, render_template, request, url_for
@@ -18,24 +19,26 @@ def index():
             prompt=generate_prompt(data),
             temperature=0,
         )
-
         #print(data)
         return redirect(url_for("index", result=response.choices[0].text))
-        
+   
+    #Get result     
     result = request.args.get("result")
+    if result is not None:
+        print(result)
+
+     #This line renders an HTML template named "index.html" and passes the value of result as a variable to the template. 
     return render_template("index.html", result=result)
 
 def generate_prompt(data):
-    return """Identify the following items from the prompt text: 
-- Is the customer expressing anger? (true or false)
-- Sentiment (negative or positive)
+  return f"""Identify the following items from the prompt text: 
+- Customer expressing anger? (true or false)
+- Sentiment (negative or positive).
 
-Format your response as a key value pairs with \
-"Sentiment" & "Anger" as the keys.
+Format your response as an object with \
+"Sentiment" and "Anger" as the keys.
 If the information isn't present, use "unknown" \
 as the value.
 Format the Anger value as a boolean.
 
-""".format(
-    data.capitalize() 
-)
+Result:""".format(data)
